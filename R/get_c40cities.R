f5906fb1e6d8d1877698120bba034d2af991809f
Pablo Tiscornia <ptiscornia@c40.org>
2024-03-15 11:50:10 -0600

    add get_c40cities function

---
+++ R/get_c40cities.R
@@ -0,0 +1,49 @@
+#' Get current and past C40 cities
+#'
+#' @return
+#' A dataset with the City, City id and if it is currently part of C40
+#' @export
+#'
+#' @examples
+#' df_c40_cities <- get_c40cities(current = TRUE)
+##~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+##                             current c40 cities                           ----
+##~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+get_c40cities <- function(current = TRUE){
+
+  ### Check if connection is already established
+  if(exists("con")){
+    con <- con
+  } else {
+    con <- c40tools::get_dw_connection()
+  }
+
+  ### Get the data
+  if(current){
+    df_c40_cities <- dplyr::tbl(
+      con,
+      dbplyr::sql(
+        "SELECT city_id, city, current_member
+    FROM dim_cities
+    WHERE current_member = 'true'
+    ORDER BY city ASC
+    "
+      )
+    ) |>
+      dplyr::collect()
+  } else {
+    df_c40_cities <- dplyr::tbl(
+      con,
+      dbplyr::sql(
+        "SELECT city_id, city, current_member
+            FROM dim_cities
+            ORDER BY city ASC
+            "
+      )
+    ) |>
+      dplyr::collect()
+  }
+
+  return(df_c40_cities)
+
+}
