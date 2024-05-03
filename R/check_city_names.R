5f2eb8ce2d47153f6a3a2ad42dfb740cd6da9da8
Pablo Tiscornia <ptiscornia@c40.org>
2024-05-03 12:17:33 -0600

    add minor city fix in `check_city_names()`

--- R/check_city_names.R
+++ R/check_city_names.R
@@ -44,6 +44,7 @@ check_city_names <- function(df, city_var_name, clean_city_name = FALSE, want_ci
                                        city_stage1 == "tel aviv" ~ "Tel Aviv-Yafo",
                                        city_stage1 == "metropolitan municipality of lima" ~ "Lima",
                                        city_stage1 == "municipio de lisboa" ~ "Lisbon",
+                                       city_stage1 == "Sú Paulo" ~ "São Paulo",
                                        .default = city)) |>
       select(-city, -city_stage1) |>
       relocate(city = new_city_name)
